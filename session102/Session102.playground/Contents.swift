5cb093c4012fd8d879c7f110bb0b814d8e3898fb
Răzvan-Gabriel Geangu <razvan.geangu@gmail.com>
2018-10-22 18:34:10 +0100

    add playground for session 102

---
+++ session102/Session102.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+/*:
+ # Recap quiz
+ Let's see how much you remember from our first session (101). Complete the next set of challenges and ask for any help if you encounter any difficulties.
+ */
+/*:
+ ## Constants
+ Find what the permeability of free space is (and its' symbol) and declare a constant that store that value. Finally, print it on the screen and include its' measurement units in the string.
+ */
+/* Your code goes here */
+/*:
+ ## Dictionaries
+ Use a data structure to represent the houses you belong to in the Department of Informatics. It should map the name of the house to some names of people representing that house. (hint: Răzvan belongs to Lovelace) Feel free to ask around who belongs to which house.
+ */
+/* Your code goes here */
+
+/*:
+ ## Loops
+ Complete the following code to be able to print the name of the house and the number of people  belonging to it. Use the words "to be" and "student/s" in your string and articulate them based on the number of people in a house. If Răzvan appears to be in a house, print something of your choice.
+ */
+/* Your code goes here */
+
+/*:
+ ## Functions
+ Create a function that verifies if a string is a palindrome. A string is a palindrome if its' mirror is effectively equal to the original (e.g. Madam, I'm Adam)
+ */
+/* Your code goes here */
+
